e26550af32e8720dd0a534f0f5b75a504b3ad785
Yoon Yeojune <youj11@naver.com>
2023-03-08 23:39:22 +0900

    no message

--- static/js/post_create.js
+++ static/js/post_create.js
@@ -34,7 +34,7 @@ function postCreate() {
 
 function getMySimpleProfile() {
     var settings = {
-        "url": "http://localhost:8080/users/profile/simple",
+        "url": "https://api.odle8.com/profile/simple",
         "method": "GET",
         "timeout": 0,
         "headers": {
